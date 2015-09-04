07acc7865c56dd75c22c0f5466d172dcb0b44e98
haifeng.xia <haifeng.xia@ele.me>
2015-09-04 23:00:49 +0800

    update eval->json

--- myIdCard_net.py
+++ myIdCard_net.py
@@ -1,25 +1,21 @@
 # coding=utf-8
 
 import urllib2
+import json
 
-url = 'http://apis.baidu.com/apistore/idservice/id?id=421127199105141930'
+value = raw_input("please enter you ID card: ")
+
+url = 'http://apis.baidu.com/apistore/idservice/id?id={}'.format(value)
 
 req = urllib2.Request(url)
 req.add_header("apikey", "331167c2bf7ced1b1328998920c293c6")
 
 resp = urllib2.urlopen(req)
 content = resp.read()
-
 content = content.decode("utf-8")
-
-print isinstance(content, unicode)
-
-result = eval(content)
-
+result = json.loads(content)
 result = result["retData"]
 
 if result:
     result = result["address"]
-    print isinstance(result, unicode)
-    result = result.decode("unicode_escape")
-    print result
+    print 'The address is: ',result
