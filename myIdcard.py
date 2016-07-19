3647a5a0c3c9b2eeb3469dd0d2f25e8af76798db
shlmoon <xiahaifeng@geekfinancer.com>
2016-07-19 12:22:40 +0800

    up...

--- myIdcard.py
+++ myIdcard.py
@@ -1,34 +1,58 @@
+# coding: utf-8
 
-#coding=utf-8
+from functools import reduce
+
 
 class IdCardCheck(object):
+
     def __init__(self):
         super(IdCardCheck, self).__init__()
-        self.IdCard = ''
+        self._IdCard = ''
 
     @property
-    def get_IdCard(self):
-        return self.IdCard
+    def IdCard(self):
+        return self._IdCard
 
-    @get_IdCard.setter
-    def set_IdCard(self, id_card):
+    @IdCard.setter
+    def IdCard(self, id_card):
         if isinstance(id_card, str) or isinstance(id_card, unicode):
             if len(id_card) == 18:
-                self.IdCard = id_card
+                self._IdCard = id_card
+                return
+
         raise ValueError('IdCard is invalid.')
 
     def set_valus_datatype(self, valus):
         if isinstance(self.IdCard, str):
             return str(valus)
-        elif isinstance(self.IdCard, unicode):
+        else:
             return unicode(valus)
-        raise ValueError('IdCard is invalid')
+
+        # elif isinstance(self.IdCard, unicode):
+        #     return unicode(valus)
 
     def get_checkCode(self):
         CountValue = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]
-        get_checkcode_key = reduce(lambda x,y:x+y, map(lambda x,y:x*y, [int(x) for x in self.IdCard[:-1]], CountValue))
+        get_checkcode_key = reduce(
+            lambda x, y: x + y,
+            map(
+                lambda x, y: x * y,
+                [int(x) for x in self.IdCard[:-1]],
+                CountValue))
+
         checkcode_key = get_checkcode_key % 11
-        CountRule = {'0': '1','1': '0','2': 'X','3': '9','4': '8','5': '7','6': '6','7': '5','8': '4','9': '3','10': '2'}
+        CountRule = {
+            '0': '1',
+            '1': '0',
+            '2': 'X',
+            '3': '9',
+            '4': '8',
+            '5': '7',
+            '6': '6',
+            '7': '5',
+            '8': '4',
+            '9': '3',
+            '10': '2'}
         get_checkcode_value = CountRule.get(str(checkcode_key), None)
         return self.set_valus_datatype(get_checkcode_value)
 
@@ -36,8 +60,7 @@ class IdCardCheck(object):
         return '%s%s' % (self.IdCard[:-1], self.get_checkCode())
 
     def IdCard_isvalid(self):
-        if not self.IdCard:
-            raise ValueError('IdCard is invalid')
         if self.IdCard[-1] == self.get_checkCode():
             return True
+
         return False
--- tests.py
+++ tests.py
@@ -1,21 +1,28 @@
+# coding: utf-8
 
 from unittest import TestCase
 from myIdcard import IdCardCheck
+
 
 class IdCardTestCase(TestCase):
     def setUp(self):
         self.ic = IdCardCheck()
 
-    def test_valid_id_card(self):
-        self.ic.IdCard = ''
-        self.assertTrue(self.ic.IdCard_isvalid())
+    def test_null_valid_idCard(self):
+        with self.assertRaises(ValueError):
+            self.ic.IdCard = ''
 
     def test_invalid_idCard_checkcode(self):
-        self.ic.IdCard = '121212121245454587'
+        self.ic.IdCard = '330726196507040011'
         self.assertFalse(self.ic.IdCard_isvalid())
 
     def test_invalid_idCard(self):
-        self.assertRaises(ValueError, lambda: self.ic.IdCard_isvalid())
+        with self.assertRaises(ValueError):
+            self.ic.IdCard = '45612323'
+
+    def test_valid_idCard(self):
+        self.ic.IdCard = '330726196507040016'
+        self.assertTrue(self.ic.IdCard_isvalid())
 
     def tearDown(self):
         self.ic = None
